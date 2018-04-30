ade0809afd9d11f943cb29e9358748eff1c9c1f3
EC2 Default User <ec2-user@ip-172-31-16-169.us-east-2.compute.internal>
2018-04-30 08:14:21 +0000

    tasklist bootstap

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,9 +3,22 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク名</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+    </table>
     
-    <p>タスク名: {{ $task->content }} > ステータス: {{$task->status}}</p>
-    
-    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
 @endsection
